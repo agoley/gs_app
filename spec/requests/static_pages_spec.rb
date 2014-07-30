2387b7a399905a8538435e3585b5d3dbbe47a51e
alex <ajgoley@gmail.com>
2014-07-30 16:14:50 -0400

    Layout and route work

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,11 +5,11 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Game Swap")
     end
   end
@@ -17,12 +17,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Game Swap | Help")
     end
   end
@@ -30,13 +30,27 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Game Swap | About")
+    end
+  end
+ 
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Game Swap | Contact")
     end
   end
 
