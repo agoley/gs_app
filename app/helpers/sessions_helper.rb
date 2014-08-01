ceb05386d2a63c898856a1297ac684c75dbed31c
alex <ajgoley@gmail.com>
2014-08-01 11:48:32 -0400

    working signin/out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
 module SessionsHelper
 
+
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.digest(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,13 +4,6 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
-    before { visit signin_path }
-
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
-  end
-
   describe "signin" do
     before { visit signin_path }
 
